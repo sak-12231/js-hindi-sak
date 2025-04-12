73015f8d120e67b26e47ee93b38e33c63d3aae89
sak-12231 <ggi2021.2161@ggi.ac.in>
2025-04-12 16:03:19 +0000

    comparsion

--- 01_basics/03_conversionorOperation.js
+++ 01_basics/03_conversionorOperation.js
@@ -1,11 +1,11 @@
 let score = "sakshi"
 
- console.log(typeof score);
- console.log(typeof (score));
+ //console.log(typeof score);
+ //console.log(typeof (score));
 
  let valueInNumber = Number(score)
- console.log(typeof valueInNumber);
- console.log(valueInNumber);
+ //console.log(typeof valueInNumber);
+ //console.log(valueInNumber);
 
 
  // 33 =>33
@@ -15,7 +15,7 @@ let score = "sakshi"
  let isLoggedIn = "sakshi"
 
  let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+//console.log(booleanIsloggedIn);
 
 
 // 1 => true; 0 => false
@@ -25,6 +25,44 @@ console.log(booleanIsloggedIn);
 let someNumber = 33
  
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+
+//***************************** Operations*****************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = "sakshi"
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1  + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console/log( (3 + 4) * 5 % 3);
+
+//console.log(+true);
+//console.log(+"");
+ let num1, num2, num3
+ num1 = num2 = num3 = 2 + 2
+ 
+ let gamecounter = 1000
+ ++gamecounter;
+ console.log(gamecounter);
+
+
+
 
