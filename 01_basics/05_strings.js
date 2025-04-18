159c3d2dd379c1a5e2d93828f9fd9abbb55e22db
sak-12231 <ggi2021.2161@ggi.ac.in>
2025-04-18 16:40:42 +0000

    strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,35 @@
+const name = "sakshi"
+const repoCount = 50
+
+//console.log( name + repoCount + "Value");
+
+console.log('Hello my name is ${name} and my repo Count is ${repoCount}');
+
+const gameName = new String('sakshi-sc-com')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4 )
+console.log(anotherString);
+
+const newStringOne = "   sakshi   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://sakshi.com/sakshi%20srivastav"
+
+console.log(url.replace('%20','-'));
+
+console.log(url.includes('sakshi'));
+
+console.log(gameName.split('-'));
