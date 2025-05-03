53cc72bb0758c392f2c39e7134b03abb85f04778
sak-12231 <ggi2021.2161@ggi.ac.in>
2025-05-03 18:20:57 +0000

    functions.js

--- 01_basics/02_basics/05_functions.js
+++ 01_basics/02_basics/05_functions.js
@@ -49,10 +49,45 @@
 
 
 loginUserMessage("sakshi")  //function execute and return a value but not to ask print or store the function, so it is not print 
-console.log(loginUserMessage("sakshi"));
-console.log(loginUserMessage("")); 
-console.log(loginUserMessage());  //undefined
+//console.log(loginUserMessage("sakshi"));
+//console.log(loginUserMessage("")); 
+//console.log(loginUserMessage());  //undefined
 
+//function calculateCarPrice(num1){
+//function calculateCarPrice(...num1){  //rest operator 
+function calculateCarPrice(val1, val2, ...num1){    //what value is going on the num1
+
+  return num1
+
+}
+ console.log(calculateCarPrice(2));
+ console.log(calculateCarPrice(200, 400, 500));
+ console.log(calculateCarPrice(200, 400, 500, 2000));
+
+
+ const user = {
+  username: "sakshi",
+  price: 199
+ }
+
+ function handleObject(anyobject){
+   console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+ }
+
+// handleObject(user)
+handleObject({
+  username:"sam",
+  price: 500
+})
+
+const myNewArray = [200, 400, 600, 800]
+
+ function returnSecondValue(getArray){
+  return getArray[1]
+ }
+
+ //console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 600, 800]));
 
    
 
