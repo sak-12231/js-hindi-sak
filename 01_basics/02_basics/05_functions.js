52e15b6cdf6f47e3d326bb819e344438383eed8e
sak-12231 <ggi2021.2161@ggi.ac.in>
2025-05-04 16:46:30 +0000

    scope.js

--- 01_basics/02_basics/05_functions.js
+++ 01_basics/02_basics/05_functions.js
@@ -8,19 +8,19 @@
  console.log("T");
  }
 
- //sayMyName()
+ sayMyName()
 
-// function addTwoNumbers(number1, number2){
-  //  console.log(number1 + number2);
+ function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
 
  //}
  
  function addTwoNumbers(number1, number2){
     
-  // let result = number1 + number2
-   //console.log("sakshi");  //function work before use return
-  // return result
-   //console.log("sakshi");  //function do not work afer  use return result
+   let result = number1 + number2
+   console.log("sakshi");  //function work before use return
+   return result
+   console.log("sakshi");  //function do not work afer  use return result
 
    return number1 + number2  //dirct value return ,it casn save space
 
@@ -29,17 +29,17 @@
 
 
 
- //addTwoNumbers(3,6)
- //addTwoNumbers(3, "6")
- //addTwoNumbers(3, "a")
- //addTwoNumbers(3, null)
+ addTwoNumbers(3,6)
+ addTwoNumbers(3, "6")
+ addTwoNumbers(3, "a")
+ addTwoNumbers(3, null)
 
  const result = addTwoNumbers(3, 6)
 
- //console.log("Result:", result);
+ console.log("Result:", result);
 
  function loginUserMessage(username = "sam"){
- //if(username === undefined){
+ if(username === undefined){
  if(!username){
   console.log("Please enter username");
   return
@@ -49,12 +49,12 @@
 
 
 loginUserMessage("sakshi")  //function execute and return a value but not to ask print or store the function, so it is not print 
-//console.log(loginUserMessage("sakshi"));
-//console.log(loginUserMessage("")); 
-//console.log(loginUserMessage());  //undefined
+console.log(loginUserMessage("sakshi"));
+console.log(loginUserMessage("")); 
+console.log(loginUserMessage());  //undefined
 
-//function calculateCarPrice(num1){
-//function calculateCarPrice(...num1){  //rest operator 
+function calculateCarPrice(num1){
+function calculateCarPrice(...num1){  //rest operator 
 function calculateCarPrice(val1, val2, ...num1){    //what value is going on the num1
 
   return num1
@@ -86,7 +86,7 @@ const myNewArray = [200, 400, 600, 800]
   return getArray[1]
  }
 
- //console.log(returnSecondValue(myNewArray));
+ console.log(returnSecondValue(myNewArray));
 console.log(returnSecondValue([200, 400, 600, 800]));
 
    
---
+++ 01_basics/02_basics/06_scope.js
@@ -0,0 +1,62 @@
+ //let a = 10
+ //const b = 20
+ //var c = 30 
+
+ //var c = 300
+ let a =300
+
+ if(true){
+    let a = 10
+    const b = 20
+    //var c = 30
+   // c = 30
+  // console.log("INNER:", a);
+ }
+   
+
+
+ 
+
+// console.log(a);
+ //console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "sakshi"
+
+    function two(){
+        website = "youtube"
+        console.log(username);
+    }
+   // console.log(website);  //website is not defined bcoz it is in outside the scope
+    // two()  //two is not call isliye execute nhi hoga
+    two()
+}
+
+//one()
+
+if(true){
+    const username = "simpy"
+    if(username === "simpy"){
+        const website = " youtube"
+        //console.log(username + website)
+    }
+    //console.log(website);
+}
+
+//console.log(username);
+
+//*****************************interesting*********************************** 
+console.log(addone(5));
+function addone(num1){     // it is function
+    return num1 + 1
+}
+
+//addone(5)
+
+addTwo(5)
+const addTwo = function(num){    // it is expression
+    return num + 2
+}
+
+//addTwo(5)
